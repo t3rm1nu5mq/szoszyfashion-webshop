b666d8d37078ebb2e07330da40c5fec623b2542b
Szabó András <terminusmq@terminusmq.app>
2021-11-09 21:48:39 +0100

    Discounts core created

---
+++ database/migrations/2021_11_09_203657_create_discounts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('title');
+            $table->longText('description');
+            $table->integer('type');
+            $table->integer('value');
+            $table->dateTime('from')->nullable();
+            $table->dateTime('to')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+}
