b0d008241a082a1572cca625032b07d7646cc66e
Szabó András <terminusmq@terminusmq.app>
2021-11-22 11:04:02 +0100

    Default button component modified

--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition primary-button">
         <slot></slot>
     </button>
 </template>
@@ -16,3 +16,12 @@
         }
     })
 </script>
+
+<style>
+    .primary-button {
+        background-color: rgba(104,88,88,255)
+    }
+    .primary-button:hover {
+        background-color: rgb(132,116,116);
+    }
+</style>
