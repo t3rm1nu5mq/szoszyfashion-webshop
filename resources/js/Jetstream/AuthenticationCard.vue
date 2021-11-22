d6e4badbc016ca160f77ec6b820dfc8fafad9736
Szabó András <terminusmq@terminusmq.app>
2021-11-22 11:05:28 +0100

    AuthenticationCard & Login component modified

--- resources/js/Jetstream/AuthenticationCard.vue
+++ resources/js/Jetstream/AuthenticationCard.vue
@@ -4,8 +4,14 @@
             <slot name="logo" />
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg  card-left-border">
             <slot />
         </div>
     </div>
 </template>
+
+<style>
+    .card-left-border {
+        border-left: 10px solid rgba(104, 88, 88, 255);
+    }
+</style>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -14,29 +14,32 @@
 
         <form @submit.prevent="submit">
             <div>
-                <jet-label for="email" value="Email" />
+                <jet-label for="email" value="E-mail cím" />
                 <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
             </div>
 
             <div class="mt-4">
-                <jet-label for="password" value="Password" />
+                <jet-label for="password" value="Jelszó" />
                 <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label class="flex items-center">
                     <jet-checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                    <span class="ml-2 text-sm text-gray-600">Emlékezz rám</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                <Link v-if="canResetPassword" :href="route('register')" class="underline text-sm text-gray-600 hover:text-gray-900" style="padding-right: 20px">
+                    Fiók létrehozása
+                </Link>
                 <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
+                    Elfelejtett jelszó
                 </Link>
 
                 <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
+                    Bejelentkezés
                 </jet-button>
             </div>
         </form>
